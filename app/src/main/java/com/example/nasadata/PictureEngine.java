bd8773b63195c6bf8b407559139bb69fa02b7e94
Sanaubia <Sanaubia@gmail.com>
2020-02-13 18:23:38 +0200

    Picture of the day done

---
+++ app/src/main/java/com/example/nasadata/PictureEngine.java
@@ -0,0 +1,84 @@
+package com.example.nasadata;
+
+import android.util.Log;
+
+import org.json.JSONObject;
+
+import java.util.Map;
+
+public class PictureEngine implements HTTPGetThread.OnRequestDoneInterface {
+
+
+    // This interface is used to report data back to UI
+    public interface PictureOftheDayUrlAvailableInterface
+    {
+        // This method is called back in background thread.
+        public void urlAvailable();
+    }
+
+
+    protected String url;
+    protected PictureOftheDayUrlAvailableInterface uiCallback;
+
+    // Constructor
+    public PictureEngine(PictureOftheDayUrlAvailableInterface callbackInterface)
+    {
+        this.uiCallback = callbackInterface;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+
+
+    public void getPictureUrl()
+    {
+        //String url = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=65dbec3aae5e5bf9000c7a956c8b76f6";
+        String url =  "https://api.nasa.gov/planetary/apod?api_key=eDKy0MMzXpklGv8pdPedrQiluatiCgjPOpKax8Yu";
+
+        HTTPGetThread getter = new HTTPGetThread(url, this);
+        getter.setListener(this);
+        getter.start();
+    }
+
+
+
+    @Override
+    public void onRequestDone(String data)
+    {
+        Log.d("LABRA dataa tulee: ", data);
+        try
+        {
+
+            Map<String, Object> parsed = JsonUtils.jsonToMap(new JSONObject(data));
+            String url = String.valueOf(parsed.get("url"));
+            this.url = url;
+            // No proper error handling here:
+           // Map<String, Object> mainElement = (Map) parsed.get("url");
+           //double tempInC = temp - KELVIN_CONVERT;
+           // this.temperature = String.format("%.1f", tempInC);
+            //ArrayList<Map<String, Object>> array = (ArrayList<Map<String, Object>>)parsed.get("weather");
+            //Map<String, Object> weatherElement = array.get(0);
+            //iconId = (String)weatherElement.get("icon");
+
+            uiCallback.urlAvailable();
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+
+    @Override
+    public void onError(String error) {
+
+    }
+
+
+}
