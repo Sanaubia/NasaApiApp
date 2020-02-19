33798457d75325b09681813309ad17f7ff27f7c6
Niko Kontu <t7koni00@students.oamk.fi>
2020-02-19 08:16:59 +0200

    ProgressBar included. Commented WeatherActivity. Cleaning the code skeleton.

--- app/src/main/java/com/example/nasadata/WeatherActivity.java
+++ app/src/main/java/com/example/nasadata/WeatherActivity.java
@@ -10,6 +10,7 @@ import android.view.View;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 import com.android.volley.Request;
@@ -30,6 +31,7 @@ public class WeatherActivity extends AppCompatActivity {
     String jsonUrl = "https://api.nasa.gov/insight_weather/?api_key=eDKy0MMzXpklGv8pdPedrQiluatiCgjPOpKax8Yu&feedtype=json&ver=1.0";
     //JSONArray sol = null;
     String celciusSign = "°C";
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,6 +46,7 @@ public class WeatherActivity extends AppCompatActivity {
         actionBar.hide();
 
         Intent intent = getIntent();
+        //Calling JsonRequest function
         JsonRequest();
 
         degrees = (TextView) findViewById(R.id.degrees);
@@ -51,8 +54,9 @@ public class WeatherActivity extends AppCompatActivity {
         wind = (TextView) findViewById(R.id.wind);
         yesterdayWeather = (Button) findViewById(R.id.btn_yesterdayWeather);
         todayWeather = (Button) findViewById(R.id.btn_todayWeather);
-
-
+        progressBar = findViewById(R.id.progressBar);
+        progressBar.setVisibility(View.VISIBLE);
+        //Set onClick Listeners and action for that
         yesterdayWeather.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -69,34 +73,37 @@ public class WeatherActivity extends AppCompatActivity {
 
     }
     public void JsonRequest(){
+        //Make a new JsonObjectRequest and that get's inside jsonUrl where we get the Json Data from Nasa
         final JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, jsonUrl, null,
                 new Response.Listener<JSONObject>() {
+
                     @Override
                     public void onResponse(JSONObject response) {
+                        progressBar.setVisibility(View.GONE);
 
                         try {
+                            //Using JsonArray. We put sol_keys inside the array. There is 7 keys (7days data)
                             JSONArray jsonArray = response.getJSONArray("sol_keys");
-                            //Log.d("RESPONSE",response.toString());
 
-                            //Log.d("temp", response.getString("435"));
+                            //Reading the data from response-data to String and using index 6 to get newest temperature data. Showing the data in TextView.
                             String temp = response.getJSONObject(jsonArray.getString(6)).getJSONObject("AT").getString("av");
                             degrees.setText("\nTemp (avg): "+temp + celciusSign +"\n");
-
+                            //Get and show the minimum temperature
                             String tempMin = response.getJSONObject(jsonArray.getString(6)).getJSONObject("AT").getString("mn");
                             degreesMin.setText("Temp (min): "+tempMin + celciusSign +"\n");
-
+                            //Get and show windSpeed
                             String windSpeed = response.getJSONObject(jsonArray.getString(6)).getJSONObject("HWS").getString("av");
-                            //Log.d("WIND",response.getJSONObject(jsonArray.getString(6)).getJSONObject("HWS").getString("av"));
                             wind.setText("Wind (avg): "+windSpeed + "m/s");
-                            //Log.d("TEMPERATURE",temp);
 
                         } catch (JSONException e) {
                             e.printStackTrace();
                         }
                     }
                 }, new Response.ErrorListener() {
+            //Error handling
             @Override
             public void onErrorResponse(VolleyError error) {
+                progressBar.setVisibility(View.GONE);
                 Toast.makeText(WeatherActivity.this, "Something went wrong",Toast.LENGTH_LONG).show();
                 error.printStackTrace();
             }
@@ -112,9 +119,7 @@ public class WeatherActivity extends AppCompatActivity {
 
                         try {
                             JSONArray jsonArray = response.getJSONArray("sol_keys");
-                            //Log.d("RESPONSE",response.toString());
-
-                            //Log.d("temp", response.getString("435"));
+                            //Reading the data from response-data to String and using index 5 to get yesterday temperature data. Showing the data in TextView.
                             String temp = response.getJSONObject(jsonArray.getString(5)).getJSONObject("AT").getString("av");
                             degrees.setText("\nTemp (avg): "+temp + celciusSign +"\n");
 
@@ -122,9 +127,8 @@ public class WeatherActivity extends AppCompatActivity {
                             degreesMin.setText("Temp (min): "+tempMin + celciusSign +"\n");
 
                             String windSpeed = response.getJSONObject(jsonArray.getString(5)).getJSONObject("HWS").getString("av");
-                            //Log.d("WIND",response.getJSONObject(jsonArray.getString(6)).getJSONObject("HWS").getString("av"));
                             wind.setText("Wind (avg): "+windSpeed + "m/s");
-                            //Log.d("TEMPERATURE",temp);
+
 
                         } catch (JSONException e) {
                             e.printStackTrace();
@@ -133,10 +137,12 @@ public class WeatherActivity extends AppCompatActivity {
                 }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
+                progressBar.setVisibility(View.GONE);
                 Toast.makeText(WeatherActivity.this, "Something went wrong",Toast.LENGTH_LONG).show();
-                error.printStackTrace();
+
             }
         });
         WeatherSingleton.getInstance(WeatherActivity.this).addToRequestqueue(jsonObjectRequest);
+
     }
 }
