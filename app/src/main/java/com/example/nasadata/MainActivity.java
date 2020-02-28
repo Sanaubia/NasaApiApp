07cd4cbb1000de22d66156a5d49c06042e97582e
Sanaubia <Sanaubia@gmail.com>
2020-02-28 12:25:19 +0200

    color themen changed

--- app/src/main/java/com/example/nasadata/MainActivity.java
+++ app/src/main/java/com/example/nasadata/MainActivity.java
@@ -3,10 +3,8 @@ package com.example.nasadata;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
